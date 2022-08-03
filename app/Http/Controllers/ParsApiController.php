9319136b28a95075e38fbda167a122d4cd6064d9
MahdiBagheri71 <MB730@rocketmail.com>
2022-08-03 16:03:28 +0430

    api token

---
+++ app/Http/Controllers/ParsApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+class ParsApiController extends Controller
+{
+    public $user;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->user = User::where('api_token', $request->input('token','')) -> first();
+
+    }
+
+    /**
+     * get info user login token api
+     * return json
+     */
+    public function getMe(Request $request)
+    {
+        return response()->json(['me' => $this->user], 200);
+    }
+}
