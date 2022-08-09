49c4cfd2e361c30b21408a0a1e5b2c72f92cbaed
MahdiBagheri71 <MB730@rocketmail.com>
2022-08-09 11:16:02 +0430

    datepicker jalai

--- resources/views/dashboard/tasksCalendar.blade.php
+++ resources/views/dashboard/tasksCalendar.blade.php
@@ -24,66 +24,127 @@
 @section('content')
 
 
-
-  <!-- Modal -->
-  <div class="modal fade" id="taskModal" tabindex="-1" aria-labelledby="taskModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          <h5 class="modal-title" id="taskModalLabel"></h5>
-        </div>
-        <div class="modal-body" style="text-align: center">
-            <input type="hidden" id="task-id">
-            <div>
-                {{__('Note')}} :
-                <pre id="note-modal"></pre>
-            </div>
-            <div>
-                {{__('Status')}} :
-                <select  class="form-select" aria-label="{{__('Status')}}" id="select-status" style="text-align: center;">
-                    <option value="cancel" style="background : #f0077f;">{{__('cancel')}}</option>
-                    <option value="success" style="background : #4cd548;">{{__('success')}}</option>
-                    <option value="retarded" style="background : #eecd18;">{{__('retarded')}}</option>
-                    <option value="doing" style="background : #2094fb;">{{__('doing')}}</option>
-                    <option value="planned" style="background : #04a1bb;">{{__('planned')}}</option>
-                    {!! Auth::user()->is_admin == 1 ?  '<option value="delete" style="background : #bf565b;">'.__('delete').'</option>' : '' !!}
-                </select>
-
-                <span id="status-modal"></span>
-
-            </div>
-            <div>
-                {{__('Date')}} :
-                <span id="date-modal"></span>
-            </div>
-            <div>
-                {{__('Time')}} :
-                <span id="time-modal"></span>
-            </div>
-            <div>
-                {{__('User')}} :
-                <span id="user_id-modal"></span>
-            </div>
-            <div>
-                {{__('Create By')}} :
-                <span id="create_by_id-modal"></span>
-            </div>
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
-          <button type="button" id="btn-save-task" class="btn btn-primary">{{__('Save')}}</button>
-        </div>
-      </div>
-    </div>
-  </div>
-
 <div class="container">
 
     <h1 style="text-align: center">{{ __('Calendar Tasks') }}</h1>
 
     <div id='calendar'></div>
+
+    <!-- Modal Edit Tasks-->
+    <div class="modal fade" id="taskModal" tabindex="-1" aria-labelledby="taskModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <h5 class="modal-title" id="taskModalLabel"></h5>
+            </div>
+            <div class="modal-body" style="text-align: center">
+                <input type="hidden" id="task-id">
+                <div>
+                    {{__('Note')}} :
+                    <pre id="note-modal"></pre>
+                </div>
+                <div>
+                    {{__('Status')}} :
+                    <select  class="form-select" aria-label="{{__('Status')}}" id="select-status" style="text-align: center;">
+                        <option value="cancel" style="background : #f0077f;">{{__('cancel')}}</option>
+                        <option value="success" style="background : #4cd548;">{{__('success')}}</option>
+                        <option value="retarded" style="background : #eecd18;">{{__('retarded')}}</option>
+                        <option value="doing" style="background : #2094fb;">{{__('doing')}}</option>
+                        <option value="planned" style="background : #04a1bb;">{{__('planned')}}</option>
+                        {!! Auth::user()->is_admin == 1 ?  '<option value="delete" style="background : #bf565b;">'.__('delete').'</option>' : '' !!}
+                    </select>
+
+                    <span id="status-modal"></span>
+
+                </div>
+                <div>
+                    {{__('Date')}} :
+                    <span id="date-modal"></span>
+                </div>
+                <div>
+                    {{__('Time')}} :
+                    <span id="time-modal"></span>
+                </div>
+                <div>
+                    {{__('User')}} :
+                    <span id="user_id-modal"></span>
+                </div>
+                <div>
+                    {{__('Create By')}} :
+                    <span id="create_by_id-modal"></span>
+                </div>
+
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
+            <button type="button" id="btn-save-task" class="btn btn-primary">{{__('Save')}}</button>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    @if(Auth::user()->is_admin == 1 )
+
+
+        <link rel="stylesheet" href="{{ url('/') }}/{{ asset('css/persian-datepicker.min.css') }}" />
+
+        <script src="{{ url('/') }}/{{ asset('js/persian-date.min.js') }}"></script>
+        <script src="{{ url('/') }}/{{ asset('js/persian-datepicker.min.js') }}"></script>
+        <!-- Modal aDD Tasks-->
+        <div class="modal fade" id="taskAddModal" tabindex="-1" aria-labelledby="taskModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h5 class="modal-title" id="taskAddModalLabel"></h5>
+                </div>
+                <div class="modal-body" style="text-align: center">
+                    <input type="hidden" id="task-add-id">
+                    <div>
+                        {{__('Note')}} :
+                        <pre id="note-add-modal"></pre>
+                    </div>
+                    <div>
+                        {{__('Status')}} :
+                        <select  class="form-select" aria-label="{{__('Status')}}" id="select-add-status" style="text-align: center;">
+                            <option value="cancel" style="background : #f0077f;">{{__('cancel')}}</option>
+                            <option value="success" style="background : #4cd548;">{{__('success')}}</option>
+                            <option value="retarded" style="background : #eecd18;">{{__('retarded')}}</option>
+                            <option value="doing" style="background : #2094fb;">{{__('doing')}}</option>
+                            <option value="planned" style="background : #04a1bb;">{{__('planned')}}</option>
+                            {!! Auth::user()->is_admin == 1 ?  '<option value="delete" style="background : #bf565b;">'.__('delete').'</option>' : '' !!}
+                        </select>
+                    </div>
+                    <div>
+                        {{__('Date')}} :
+
+                          <input type="text" class="date-add-modal"  />
+                          <input id="date-add-modal" class="form-control" type="hidden">
+
+                    </div>
+                    <div>
+                        {{__('Time')}} :
+                        <input type="text" class="time-add-modal" id="time-add-modal" />
+                    </div>
+                    <div>
+                        {{__('User')}} :
+                        <span id="user_id-add-modal"></span>
+                    </div>
+                    <div>
+                        {{__('Create By')}} :
+                        <span id="create_by_id-add-modal"></span>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{__('Close')}}</button>
+                <button type="button" id="btn-save-task" class="btn btn-primary">{{__('Save')}}</button>
+                </div>
+            </div>
+            </div>
+        </div>
+    @endif
 
 </div>
 
@@ -93,7 +154,40 @@
 
 $(document).ready(function () {
 
+    @if(Auth::user()->is_admin == 1 )
+        $(".date-add-modal").pDatepicker(
+            {
+                initialValue: false,
+                observer: true,
+                format: 'YYYY-MM-DD',
+                timePicker: {
+                    enabled: false
+                },
+                altFieldFormatter : function(val){
+                    var date  = new Date(val);
+                    var date_format = date.getFullYear()+ '-' + (date.getMonth()+1) + "-" + date.getDate();
+                    $('#date-add-modal').val(date_format);
+                    // console.log(date_format)
+                }
+        });
 
+        $(".time-add-modal").pDatepicker(
+            {
+                onlyTimePicker: true,
+                timePicker: {
+                    meridian: {
+                        enabled: true
+                    },
+                    second: {
+                        enabled: false
+                    }
+                },
+                format: 'HH:mm:ss'
+        });
+
+
+
+    @endif
 
     var SITEURL = "{{ url('/') }}";
 
@@ -181,63 +275,11 @@ $(document).ready(function () {
 
         select: function (start, end, allDay) {
 
-            var title = prompt('Event Title:');
+            // var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
 
-            if (title) {
+            // var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
 
-                var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
-
-                var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
-
-                $.ajax({
-
-                    url: SITEURL + "/fullcalenderAjax",
-
-                    data: {
-
-                        title: title,
-
-                        start: start,
-
-                        end: end,
-
-                        type: 'add'
-
-                    },
-
-                    type: "POST",
-
-                    success: function (data) {
-
-                        displayMessage("Event Created Successfully");
-
-
-
-                        calendar.fullCalendar('renderEvent',
-
-                            {
-
-                                id: data.id,
-
-                                title: title,
-
-                                start: start,
-
-                                end: end,
-
-                                allDay: allDay
-
-                            },true);
-
-
-
-                        calendar.fullCalendar('unselect');
-
-                    }
-
-                });
-
-            }
+            $('#taskAddModal').modal('show');
 
         },
 
